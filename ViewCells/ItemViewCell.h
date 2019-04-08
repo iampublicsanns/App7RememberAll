af9f0def2a269aa38a6d1397c284987ff3d36e00
Alexander <alexander@alexanders-macbook-pro.local>
2019-04-08 13:29:08 +0300

    Just added collection view cell subclass.

---
+++ ViewCells/ItemViewCell.h
@@ -0,0 +1,17 @@
+//
+//  ItemViewCell.h
+//  App7RememberAll
+//
+//  Created by Alexander on 08/04/2019.
+//  Copyright © 2019 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ItemViewCell : UICollectionViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ViewCells/ItemViewCell.m
@@ -0,0 +1,13 @@
+//
+//  ItemViewCell.m
+//  App7RememberAll
+//
+//  Created by Alexander on 08/04/2019.
+//  Copyright © 2019 Alexander. All rights reserved.
+//
+
+#import "ItemViewCell.h"
+
+@implementation ItemViewCell
+
+@end
