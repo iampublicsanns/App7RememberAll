1d249a90357ffd20cc0ac9965eaf4324a24cf1cd
Alexander <alexander@alexanders-macbook-pro.local>
2019-04-08 13:14:21 +0300

    Just added a collection vc subclass.

---
+++ ViewControllers/GalleryVC.h
@@ -0,0 +1,17 @@
+//
+//  GalleryVC.h
+//  App7RememberAll
+//
+//  Created by Alexander on 08/04/2019.
+//  Copyright © 2019 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GalleryVC : UICollectionViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
