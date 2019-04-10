adf58d94a18afaf059db543e55a526aff249a720
Alexander <alexander@alexanders-macbook-pro.local>
2019-04-10 11:56:13 +0300

    In ItemViewCell: adding an imageView.

--- ViewCells/ItemViewCell.h
+++ ViewCells/ItemViewCell.h
@@ -11,7 +11,8 @@
 NS_ASSUME_NONNULL_BEGIN
 
 @interface ItemViewCell : UICollectionViewCell
-
+//@property (weak, nonatomic)
+@property (nonatomic) UIImage* image;
 @end
 
 NS_ASSUME_NONNULL_END
--- ViewCells/ItemViewCell.m
+++ ViewCells/ItemViewCell.m
@@ -8,7 +8,37 @@
 
 #import "ItemViewCell.h"
 
+@interface ItemViewCell ()
+@property (nonatomic) UIImageView *imageView;
+@end
+
 @implementation ItemViewCell
+
+// not called
+- (instancetype) init {
+  if ([super init] == nil) return nil;
+  
+  self.imageView = [[UIImageView alloc] init];
+  [self.contentView addSubview: self.imageView];
+  
+  return self;
+}
+
+-(void) createImageView {
+  self.imageView = [[UIImageView alloc] init];
+  [self.contentView addSubview: self.imageView];
+  
+  //positioning
+  
+}
+  
+-(void) setImage:(UIImage*) image {
+  if (self.imageView == nil) [self createImageView];
+  
+  //self.image = image;
+  self.imageView.image = image;
+  [self.imageView sizeToFit];
+}
 
 -(NSString *) description {
   return [NSString stringWithFormat:@"Sanz -- %@", super.description];
