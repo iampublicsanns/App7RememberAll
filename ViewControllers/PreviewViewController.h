98c4abc05c4bf8e50acb7c52ae929068425585b4
Alexander <alexander@alexanders-macbook-pro.local>
2019-04-11 13:19:58 +0300

    Just created a PreviewViewController

---
+++ ViewControllers/PreviewViewController.h
@@ -0,0 +1,17 @@
+//
+//  PreviewViewController.h
+//  App7RememberAll
+//
+//  Created by Alexander on 11/04/2019.
+//  Copyright © 2019 Alexander. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PreviewViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
