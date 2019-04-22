44edd6e9ac949ca269e5fbf1a0b4003a0570072c
Alexander <alexander@alexanders-macbook-pro.local>
2019-04-22 16:35:08 +0300

    Made some static methods public.

--- ViewControllers/GalleryVC.h
+++ ViewControllers/GalleryVC.h
@@ -12,6 +12,12 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface GalleryVC : UICollectionViewController
 - (instancetype)initWithCollectionViewLayout:(UICollectionViewLayout *)layout;
+
++ (void)asyncGetImage:(NSDictionary*)json
+           completion:(void(^)(UIImage*))completion;
++ (void)asyncGetImageByUrl:(NSString*)url
+                completion:(void(^)(UIImage*))completion;
++ (NSMutableDictionary*)cachedImages;
 @end
 
 NS_ASSUME_NONNULL_END
