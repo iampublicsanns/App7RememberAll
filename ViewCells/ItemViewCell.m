9e14a57c2e708456fa05838c13b69a2e2ba20b69
alexander <a@a.ru>
2019-04-28 13:14:15 +0300

    Clearing

--- ViewCells/ItemViewCell.m
+++ ViewCells/ItemViewCell.m
@@ -18,39 +18,6 @@
 
 @implementation ItemViewCell
 
-// never called
-- (instancetype) init {
-  if ([super init] == nil) return nil;
-  
-  self.imageView = [[UIImageView alloc] init];
-  [self.contentView addSubview: self.imageView];
-  
-  return self;
-}
-
--(void) makeButton {
-  if (self.button == nil){
-    self.button = [UIButton buttonWithType:UIButtonTypeCustom];
-
-    [self.button setTitle:@"A button"
-                 forState:UIControlStateNormal];
-    [self.button sizeToFit];
-    
-    CGSize parentSize = self.frame.size;
-    self.button.frame = CGRectMake(0, 0, parentSize.width, parentSize.height);
-    
-    //coursera Intro to UIImageView
-    self.button.layer.borderWidth = 2;
-    self.button.layer.borderColor = [UIColor colorWithRed:0 green:0 blue:1 alpha:1].CGColor;
-    
-    
-    [self.contentView addSubview: self.button];
-    
-    [self.button addTarget:self
-                    action:@selector(handleClick)
-          forControlEvents:UIControlEventTouchUpInside];
-  }
-}
 
 -(void) createImageView {
   self.imageView = [[UIImageView alloc] init];
@@ -63,11 +30,6 @@
   self.imageView.layer.borderWidth = 1;
   // instead of autofix:
   self.imageView.layer.borderColor = [UIColor colorWithRed:1 green:0.6 blue:0 alpha:1].CGColor;
-  
-  //positioning
-  self.imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-  
-  self.imageView.userInteractionEnabled = YES;
   
   UITapGestureRecognizer *singleTap =  [[UITapGestureRecognizer alloc]
                                         initWithTarget:self
@@ -91,37 +53,17 @@
 -(void) setImage:(UIImage*) image {
   [self setupViews];
   
-  [self.button setImage:image
-               forState:UIControlStateNormal];
-//  [self.button setBackgroundImage: image
-//               forState:UIControlStateNormal];
-  
-  //self.image = image;
   self.imageView.image = image;
-  //From docs UIImageView.image :
-  //Sets the frame of imageView :
-  //[self.imageView sizeToFit];
-  
-  //[self sizeThatFits:self.bounds.size];
   
   CGSize parentSize = self.frame.size;
   
   //https://www.youtube.com/watch?v=qV4gHfqwFPU
   self.imageView.frame = CGRectMake(0, 0, parentSize.width, parentSize.height);
-
-  
-  //  self.imageView.bounds = CGRect{
-//    origin = CGPoint {
-//      x = 5;
-//      y=6;
-//    }
-//  }
 }
 
 
 -(void)setupViews {
   if (self.imageView == nil) [self createImageView];
-  //[self makeButton];
   
   CGSize parentSize = self.frame.size;
   
