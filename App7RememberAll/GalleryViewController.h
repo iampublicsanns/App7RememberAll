a938f5c9261fc4fb32d5b79d9006fccd29ef7b38
alexander <a@a.ru>
2019-06-06 16:15:05 +0300

    2-column layout

--- App7RememberAll/GalleryViewController.h
+++ App7RememberAll/GalleryViewController.h
@@ -13,8 +13,10 @@
 NS_ASSUME_NONNULL_BEGIN
 
 
-@interface GalleryViewController : UICollectionViewController
-
+/**
+ Вью-контроллер, отрисовывающий данные, полученные из DataManager.
+ */
+@interface GalleryViewController : UICollectionViewController<UICollectionViewDelegateFlowLayout>
 
 /**
  Инициализатор
